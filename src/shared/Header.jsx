0b0c43e58a791b43041319637bbd1ff5d648b1b7
Tryphena P <tryphenap@Tryphenas-MacBook-Pro.local>
2026-06-20 11:14:41 -0700

    Advanced state, useReducer, useContext continued

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,7 +1,17 @@
+import { useAuth } from '../contexts/AuthContext';
+
 function Header() {
+    const { isAuthenticated , logout } = useAuth();
+
   return (
     <header>
       <h1>Todo List</h1>
+      {isAuthenticated && (
+       
+        <button onClick={logout}>
+          Logout
+        </button>
+      )}
     </header>
   );
 }
